c89dbd63185d057eae46fdd67f18f978649bc729
MostafaAmrHamed <Mostafaamro5619@gmail.com>
2023-06-24 22:42:02 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Pages/Navbar/Navbar";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
